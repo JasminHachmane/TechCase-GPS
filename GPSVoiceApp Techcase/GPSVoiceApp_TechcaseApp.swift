2cfec99432396846b2083be51b9b47878e4f6f0f
Jasmin Hachmane <509304@student.fontys.nl>
2025-04-01 21:41:18 +0200

    Initial Commit

---
+++ GPSVoiceApp Techcase/GPSVoiceApp_TechcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSVoiceApp_TechcaseApp.swift
+//  GPSVoiceApp Techcase
+//
+//  Created by Jasmin Hachmane on 01/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct GPSVoiceApp_TechcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
